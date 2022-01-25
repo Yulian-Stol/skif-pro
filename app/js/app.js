0dd13e1e2681e81d213114badafdbd71ed98c669
Yulian <kokomoko12345koko@gmail.com>
2022-01-25 17:03:23 +0200

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,483 @@
+'use strict';
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+		//----------------------HAMBURGER-----------------------
+		const hamburger = (hamburgerButton, hamburgerNav, hamburgerHeader) => {
+			const button = document.querySelector(hamburgerButton),
+						nav = document.querySelector(hamburgerNav),
+						header = document.querySelector(hamburgerHeader);
+	
+			button.addEventListener('click', (e) => {
+				button.classList.toggle('hamburger--active');
+				nav.classList.toggle('header__nav--active');
+				header.classList.toggle('header--menu');
+			});
+	
+		};
+		hamburger('.hamburger', '.header__nav', '.header');
+
+	//----------------------SCROLL-----------------------
+		const scrollTo = (scrollTo) => {
+			let list = document.querySelector(scrollTo);
+			list = '.' + list.classList[0]  + ' li a[href^="#"';
+	
+			document.querySelectorAll(list).forEach(link => {
+	
+				link.addEventListener('click', function(e) {
+						e.preventDefault();
+						const scrollMenu = document.querySelector(scrollTo);
+	
+						let href = this.getAttribute('href').substring(1);
+	
+						const scrollTarget = document.getElementById(href);
+	
+						// const topOffset = scrollMenu.offsetHeight;
+						const topOffset = 70;
+						const elementPosition = scrollTarget.getBoundingClientRect().top;
+						const offsetPosition = elementPosition - topOffset;
+	
+						window.scrollBy({
+								top: offsetPosition,
+								behavior: 'smooth'
+						});
+	
+						
+						let button = document.querySelector('.hamburger'),
+								nav = document.querySelector('.header__nav'),
+								header = document.querySelector('.header');
+	
+						button.classList.remove('hamburger--active');
+						nav.classList.remove('header__nav--active');
+						header.classList.remove('header--menu');
+				});
+			});
+		};
+		// scrollTo('.header__nav');
+
+		//------------------------------ACCORDIONS---------------------------
+		const accordions = (accordionSelector) => {
+			const	accordion = document.querySelectorAll(accordionSelector);
+
+			accordion.forEach(item => {
+				const accordionClick = item.querySelector('.accordion__header'),
+							accordionContent = item.querySelector('.accordion__content');
+
+				accordionClick.addEventListener('click', (e) => {
+					if(!item.classList.contains('accordion--active')) {
+
+						item.classList.add('accordion--active')
+						accordionContent.style.height = "auto"
+						var height = accordionContent.clientHeight + "px"
+						accordionContent.style.height = "0px"
+
+						setTimeout(() => {
+							accordionContent.style.height = height
+						}, 0)
+
+						} else {
+							accordionContent.style.height = "0px"
+								item.classList.remove('accordion--active')
+					}
+
+				});
+			});
+
+		};
+		accordions('.accordion');
+
+		//----------------------TABS-JS----------------------
+		
+		const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
+			const header = document.querySelector(headerSelector),
+						tab = document.querySelectorAll(tabSelector),
+						content = document.querySelectorAll(contentSelector);
+			if (document.documentElement.clientWidth < 1200) {
+			function hideTabContent() {
+				content.forEach(item => {
+					item.style.display = "none";
+				});
+
+				tab.forEach(item => {
+					item.classList.remove(activeClass);
+				});
+			}
+
+			function showTabContent(i = 0) {
+				content[i].style.display = "block";
+				tab[i].classList.add(activeClass);
+			}
+
+			hideTabContent();
+			showTabContent();
+
+			header.addEventListener('click', (e) => {
+				const target = e.target;
+				if (target && 
+					(target.classList.contains(tabSelector.replace(/\./, '')) || 
+					target.parentNode.classList.contains(tabSelector.replace(/\./, '')))) {
+					tab.forEach((item, i) => {
+						if (target == item || target.parentNode == item) {
+							hideTabContent();
+							showTabContent(i);
+						}
+					});
+				}
+			});
+		};
+		};
+		tabs('.tabs', '.tabs__item', '.tabs__wrap', 'active');
+
+
+	
+	//----------------------FIXED-HEADER-----------------------
+		const headerFixed = (headerFixed, headerActive) => {
+			const header =  document.querySelector(headerFixed),
+						active = headerActive.replace(/\./, '');
+	
+			window.addEventListener('scroll', function() {
+				const top = pageYOffset;
+				
+				if (top >= 90) {
+					header.classList.add(active);
+				} else {
+					header.classList.remove(active);
+				}
+	
+			});
+	
+		};
+		headerFixed('.header', '.header--active');
+	
+
+
+		//---------------slider--example----------
+		var mySwiper = new Swiper('.example__slider', {
+			slidesPerView: 1,
+			spaceBetween: 65,
+			mousewheel: {
+				invert: true,
+			},
+			pagination: {
+				el: '.example__pagination',
+				clickable: 'true',
+			},
+			breakpoints: {
+				767: {
+					slidesPerView: 2,
+					spaceBetween: 65
+				},
+			}
+		});
+
+		//---------------slider--example----------
+		var mySwiper = new Swiper('.team__slider', {
+			slidesPerView: 1.3,
+			spaceBetween: 10,
+			grabCursor: true,
+			freeMode: {
+				enabled: true,
+				sticky: true,
+			},
+			pagination: {
+				el: '.team__pagination',
+				clickable: 'true',
+			},
+			breakpoints: {
+				1780: {
+					slidesPerView: 4.7,
+					spaceBetween: 40
+				},
+				1500: {
+					slidesPerView: 4.3,
+				},
+				1200: {
+					slidesPerView: 3,
+				},
+				992: {
+					slidesPerView: 2.7,
+				},
+				767: {
+					slidesPerView: 2.2,
+				},
+				576: {
+					slidesPerView: 1.9,
+				},
+				360: {
+					slidesPerView: 1.5,
+					freeMode: {
+						enabled: false,
+						sticky: false,
+					},
+				}
+			}
+		});
+		
+
+		//---------------partner-slider------------
+		
+		var mySwiper = new Swiper('.using__slider', {
+			slidesPerView: 2,
+			spaceBetween: 15,
+			grabCursor: true,
+			autoplay: false,
+			loop: false,
+			grid: {
+				rows: 2,
+			},
+			pagination: {
+				el: '.using__pagination',
+				clickable: 'true',
+			},
+			breakpoints: {
+				767: {
+					slidesPerView: 4,
+					spaceBetween: 20,
+					grid: {
+						rows: 1,
+					},
+				},
+				1200: {
+					spaceBetween: 40,
+					slidesPerView: 5,
+					loop: true,
+					autoplay: true,
+					grid: {
+						rows: 1,
+					},
+				},
+			}
+		});
+
+		//-------------technology--slider--
+		if (document.documentElement.clientWidth < 767) {
+
+			var mySwiper = new Swiper('.using__sliderbot', {
+				slidesPerView: 2,
+				spaceBetween: 15,
+				grabCursor: true,
+				autoplay: false,
+				loop: false,
+				grid: {
+					rows: 2,
+				},
+				pagination: {
+					el: '.using__paginationbot',
+					clickable: 'true',
+				},
+			});
+		}
+
+
+
+	//----------------------MODAL-----------------------
+		const modals = (modalSelector) => {
+			const	modal = document.querySelectorAll(modalSelector);
+
+			if (modal) {
+				let i = 1;
+
+				modal.forEach(item => {
+					const wrap = item.id;
+					const link = document.querySelectorAll('.' + wrap);
+
+					link.forEach(linkItem => {
+						let close = item.querySelector('.close');
+							if (linkItem) {
+								linkItem.addEventListener('click', (e) => {
+									if (e.target) {
+										e.preventDefault();
+									}
+									item.classList.add('active');
+								});
+							}
+
+							if (close) {
+								close.addEventListener('click', () => {
+									item.classList.remove('active');
+								});
+							}
+
+						item.addEventListener('click', (e) => {
+							if (e.target === item) {
+								item.classList.remove('active');
+							}
+						});
+					});
+				});
+			}
+
+		};
+		modals('.modal');
+
+	//----------------------FORM-----------------------
+		const forms = (formsSelector) => {
+		const form = document.querySelectorAll(formsSelector);
+		let i = 1;
+		let img = 1;
+		let lebel = 1;
+		let prev = 1;
+
+		form.forEach(item => {
+			const elem = 'form--' + i++;
+			item.classList.add(elem);
+
+			let formId = item.id = (elem);
+			let formParent = document.querySelector('#' + formId);
+
+			formParent.addEventListener('submit', formSend);
+
+			async function formSend(e) {
+				e.preventDefault();
+
+				let error = formValidate(item);
+
+				let formData = new FormData(item);
+
+				if (error === 0) {
+					item.classList.add('_sending');
+					let response = await fetch('sendmail.php', {
+						method: 'POST',
+						body: formData
+					});
+
+					if (response.ok) {
+						let modalThanks = document.querySelector('#modal__thanks');
+						formParent.parentNode.style.display = 'none';
+
+						modalThanks.classList.add('active');
+						item.reset();
+						item.classList.remove('_sending');
+					} else {
+						alert('Ошибка при отправке');
+						item.classList.remove('_sending');
+					}
+
+				}
+			}
+
+			function formValidate(item) {
+				let error = 0;
+				let formReq = formParent.querySelectorAll('._req');
+
+				for (let index = 0; index < formReq.length; index++) {
+					const input = formReq[index];
+					// formRemoveError(input);
+
+					if (input.classList.contains('_email')) {
+						if(emailTest(input)) {
+							formAddErrorEmail(input);
+							error++;
+						}
+					} else if (input.getAttribute('type') === 'checkbox' && input.checked === false) {
+						formAddErrorCheck(input);
+						error++;
+					} else {
+						if (input.value === '') {
+							formAddError(input);
+							error++;
+						}
+					}
+				}
+				return error;
+			}
+
+			const formImgFile = formParent.querySelectorAll('.formImgFile');
+
+			formImgFile.forEach(item => { 
+				const elem = 'formImgFile--' + i++;
+
+				let formId = item.id = (elem);
+				let formParent = document.querySelector('#' + formId);
+
+				const formImage = formParent.querySelector('.formImage');
+				const formLebel = formParent.querySelector('.formLebel');
+				const formPreview = formParent.querySelector('.formPreview');
+
+				//картинка в форме
+				let formImageNumber = 'formImage--' + img++;
+				let formPreviewNumber = 'formPreview--' + prev++;
+				
+				formImage.id = (formImageNumber);
+				formLebel.htmlFor = ('formImage--' + lebel++);
+				formPreview.id = (formPreviewNumber);
+				const formImageAdd = document.querySelector('#' + formImageNumber);
+
+				// изменения в инпуте файл
+				formImageAdd.addEventListener('change', () =>  {
+					uploadFile(formImage.files[0]);
+				});
+
+				function uploadFile(file) {
+			
+					if (!['image/jpeg', 'image/png', 'image/gif', 'image/ico', 'application/pdf'].includes(file.type)) {
+						alert('Только изображения');
+						formImage.value = '';
+						return;
+					}
+			
+					if (file.size > 2 * 1024 * 1024) {
+						alert('Размер менее 2 мб.');
+						return;
+					}
+			
+					var reader = new FileReader();
+					reader.onload = function (e) {
+						if(['application/pdf'].includes(file.type)) {
+							formPreview.innerHTML = `Файл выбран`;
+						}else{
+							formPreview.innerHTML = `<img src="${e.target.result}" alt="Фото">`;
+						}
+						
+					};
+					reader.onerror = function (e) {
+						alert('Ошибка');
+					};
+					reader.readAsDataURL(file);
+				}
+			})
+
+			function formAddError(input) {
+				let div = document.createElement('div');
+				div.classList.add("form__error");
+				div.innerHTML = "Введите данные в поле";
+
+				input.parentElement.append(div);
+				input.parentElement.classList.add('_error');
+				input.classList.add('_error');
+			}
+
+			function formAddErrorEmail(input) {
+				let div = document.createElement('div');
+				div.classList.add("form__error");
+				div.innerHTML = "Введите свою почту";
+
+				input.parentElement.append(div);
+				input.parentElement.classList.add('_error');
+				input.classList.add('_error');
+			}
+
+			function formAddErrorCheck(input) {
+				let div = document.createElement('div');
+				div.classList.add("form__error");
+				div.innerHTML = "Согласие на обработку персональных данных";
+
+				input.parentElement.append(div);
+				input.parentElement.classList.add('_error');
+				input.classList.add('_error');
+			}
+
+			function emailTest(input) {
+				return !/^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/. test(input.value);
+			}
+
+		});
+		};
+		forms('.form');
+
+});
+
+
+	
